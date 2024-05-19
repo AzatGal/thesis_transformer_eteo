07dc1f2356441227b4cb5bc9419048cd43774982
Azatgal <azat_gal@icloud.com>
2024-05-19 20:38:51 +0300

    commit

--- trademaster/nets/eteo.py
+++ trademaster/nets/eteo.py
@@ -5,6 +5,38 @@ from .builder import NETS
 from .custom import Net
 
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 10):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+
+def build_transformer(_num_layers, _d_model, _nhead, _dim_feedforward=2048, _dropout=0.1, _batch_first=False):
+    _decoder = nn.TransformerDecoderLayer(
+        d_model=_d_model,
+        nhead=_nhead,
+        dim_feedforward=_dim_feedforward,
+        dropout=_dropout,
+        batch_first=_batch_first
+    )
+    return nn.TransformerDecoder(_decoder, _num_layers)
+
+
 def build_mlp(dims: [int]) -> nn.Sequential:  # MLP (MultiLayer Perceptron)
     net_list = []
     for i in range(len(dims) - 1):
@@ -15,6 +47,59 @@ def build_mlp(dims: [int]) -> nn.Sequential:  # MLP (MultiLayer Perceptron)
 
 @NETS.register_module()
 class ETEOStacked(Net):
+    def __init__(self,
+                 num_layers,
+                 d_model,
+                 nhead,
+                 dim_feedforward=2048,
+                 dropout=0.1,
+                 batch_first=False,
+                 time_steps=10
+                 ):
+        super(ETEOStacked, self).__init__()
+
+        self.net = build_transformer(num_layers,
+                                     d_model,
+                                     nhead,
+                                     dim_feedforward,
+                                     dropout,
+                                     batch_first
+                                     )
+
+        self.pos_encoding = PositionalEncoding(d_model, dropout, time_steps)
+
+        self.act_linear_volume = nn.Linear(d_model * time_steps, 2)
+        self.act_linear_price = nn.Linear(d_model * time_steps, 2)
+        self.v_linear = nn.Linear(d_model * time_steps, 1)
+
+        # init weights
+        # self.net.apply(self.init_weights)
+        self.act_linear_volume.apply(self.init_weights)
+        self.act_linear_price.apply(self.init_weights)
+        self.v_linear.apply(self.init_weights)
+
+    def forward(self, x):
+        B = x.shape
+        x = x.view(B[-1], B[0], B[-2])
+
+        x = self.pos_encoding(x)
+
+        x = self.net(x)
+        x = x.view(B[0], -1)
+
+        action_volume = self.act_linear_volume(x)
+        action_price = self.act_linear_price(x)
+        v = self.v_linear(x)
+
+        return action_volume, action_price, v
+    def init_weights(self, m):
+        # init linear
+        if isinstance(m, nn.Linear):
+            torch.nn.init.kaiming_uniform(m.weight)
+            m.bias.data.zero_()
+
+
+"""
     def __init__(self, dims: [int], state_dim: int, action_dim: int, time_steps=10, explore_rate=0.25):
         # nodes is a list where the element reprensents the nodes on each layer
         super(ETEOStacked, self).__init__()
@@ -42,9 +127,4 @@ class ETEOStacked(Net):
         v = self.v_linear(x)
 
         return action_volume, action_price, v
-
-    def init_weights(self, m):
-        # init linear
-        if isinstance(m, nn.Linear):
-            torch.nn.init.kaiming_uniform(m.weight)
-            m.bias.data.zero_()
+"""
