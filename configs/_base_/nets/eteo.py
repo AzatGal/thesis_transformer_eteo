5a2379c6e9b6f2e404d1191d7c97ce9e3cdb3e3c
Azatgal <azat_gal@icloud.com>
2024-05-19 21:04:20 +0300

    commit

--- configs/_base_/nets/eteo.py
+++ configs/_base_/nets/eteo.py
@@ -1,4 +1,27 @@
+act = dict(
+    type="ETEOStacked",
+    num_layers=3,
+    d_model=156,
+    nhead=3,
+    dim_feedforward=2048,
+    dropout=0.1,
+    batch_first=False,
+    time_steps=10
+)
 
+cri = dict(
+    type="ETEOStacked",
+    num_layers=3,
+    d_model=156,
+    nhead=3,
+    dim_feedforward=2048,
+    dropout=0.1,
+    batch_first=False,
+    time_steps=10
+)
+
+
+"""
 act = dict(
     type="ETEOStacked",
     dims=[128,128],
@@ -16,3 +39,4 @@ cri = dict(
     state_dim =10,
     explore_rate=0.25
 )
+"""
--- configs/order_execution/order_execution_BTC_eteo_eteo_adam_mse.py
+++ configs/order_execution/order_execution_BTC_eteo_eteo_adam_mse.py
@@ -228,6 +228,29 @@ optimizer = dict(type='Adam', lr=0.001)
 
 act = dict(
     type="ETEOStacked",
+    num_layers=3,
+    d_model=156,
+    nhead=3,
+    dim_feedforward=2048,
+    dropout=0.1,
+    batch_first=False,
+    time_steps=10
+)
+
+cri = dict(
+    type="ETEOStacked",
+    num_layers=3,
+    d_model=156,
+    nhead=3,
+    dim_feedforward=2048,
+    dropout=0.1,
+    batch_first=False,
+    time_steps=10
+)
+
+"""
+act = dict(
+    type="ETEOStacked",
     dims=[128,128],
     time_steps=10,
     action_dim=2,
@@ -243,3 +266,4 @@ cri = dict(
     state_dim =10,
     explore_rate=0.25
 )
+"""
