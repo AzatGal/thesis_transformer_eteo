394f4cdd5443f734d7160ad0b2051feb739af9e4
Azatgal <azat_gal@icloud.com>
2024-05-23 21:42:10 +0300

    commit

--- trademaster/nets/eiie.py
+++ trademaster/nets/eiie.py
@@ -42,10 +42,14 @@ class EIIETrans(Net):
         print(x.shape)
         x = x.transpose(1, 2)
         B = x.shape
+        self.pos_embedding = self.pos_embedding.repeat(B[0])
+        self.cls_token = self.cls_token.repeat(B[0])
         x = x.reshape(B[0], B[1] * B[2], B[3])
+        """
         print(self.pos_embedding.shape)
         print(self.cls_token.shape)
         print(x.shape)
+        """
         x = x + self.pos_embedding
         x = torch.cat((self.cls_token, x), 1)
 
