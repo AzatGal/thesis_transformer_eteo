0b3a3413bc0fc9154ecba6b6102d0651982a3eaf
Azatgal <azat_gal@icloud.com>
2024-05-26 11:51:06 +0300

    commit

--- configs/_base_/nets/eiie.py
+++ configs/_base_/nets/eiie.py
@@ -1,4 +1,4 @@
-
+"""
 act = dict(
     type="EIIETrans",
     d_model=11,
@@ -11,21 +11,20 @@ act = dict(
 
 """
 act = dict(
-    type = "EIIEConv",
-    input_dim = None,
+    type="EIIEConv",
+    input_dim=None,
     output_dim=1,
     time_steps=None,
     kernel_size=3,
-    dims = [32]
+    dims=[32]
 )
-"""
 
 cri = dict(
-    type = "EIIECritic",
-    input_dim = None,
-    action_dim = None,
+    type="EIIECritic",
+    input_dim=None,
+    action_dim=None,
     output_dim=1,
     time_steps=None,
-    num_layers = 1,
+    num_layers=1,
     hidden_size=32
 )
