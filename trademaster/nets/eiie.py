ff971a32023014268df736cbb8c6808b39a59c60
Azatgal <azat_gal@icloud.com>
2024-05-28 19:35:23 +0300

    commit

--- trademaster/nets/eiie.py
+++ trademaster/nets/eiie.py
@@ -122,22 +122,18 @@ class EIIECritic(Net):
         # print("lstm", x.shape, a.shape)
         if len(x.shape) >= 4:
             x = x.view(x.shape[0], x.shape[1], -1)
-        # print(x.shape)
+
         lstm_out, _ = self.lstm(x)
-        # print(x.shape)
         x = self.linear1(lstm_out)
-        # print(x.shape)
         x = self.act(x)
 
         x = x.view(x.shape[0], -1)
-        # print(x.shape)
         para = self.para.repeat(x.shape[0], 1)
-        # print(x.shape, para.shape, a.shape)
+
         x = torch.cat((x, para, a), dim=1)
-        x = self.linear2(x)
-        # print(x.shape)
+        # x = self.linear2(x)
         x = x.mean(dim=1, keepdim=True)
-        # print(x.shape)
+
         return x
 
 
@@ -197,5 +193,39 @@ class EIIETransCritic(Net):
         x = torch.cat((x, para, a), dim=1)
 
         x = self.linear3(x)
+
+        return x
+
+@NETS.register_module()
+class EIIEConvCritic(Net):
+    def __init__(self,
+                 input_dim,
+                 output_dim=1,
+                 time_steps=10,
+                 kernel_size=3,
+                 dims=(32,)):
+        super(EIIEConvCritic, self).__init__()
+
+        self.kernel_size = kernel_size
+        self.time_steps = time_steps
+
+        self.net = build_conv2d(
+            dims=[input_dim, *dims, output_dim],
+            kernel_size=[(1, self.kernel_size), (1, self.time_steps - self.kernel_size + 1)]
+        )
+        self.para = torch.nn.Parameter(torch.ones(1).requires_grad_())
+        self.linear = nn.Linear(2 * 30, 1)
+
+    def forward(self, x, a):  # (batch_size, num_seqs, action_dim, time_steps, state_dim)
+        if len(x.shape) > 4:
+            x = x.squeeze(1)
+        x = x.permute(0, 3, 1, 2)
+        x = self.net(x)
+        x = x.view(x.shape[0], -1)
+
+        para = self.para.repeat(x.shape[0], 1)
+        x = torch.cat((x, para, a), dim=1)
+
+        x = self.linear(x)
 
         return x
